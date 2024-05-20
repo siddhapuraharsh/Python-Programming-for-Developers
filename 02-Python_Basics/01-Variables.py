9d11c70aae4706b9b2c39f1a7d43278419afd8ac
Harsh Siddhapura <harshsiddhapura200@gmail.com>
2024-05-19 21:46:41 -0700

    Variables code added

---
+++ 02-Python_Basics/01-Variables.py
@@ -0,0 +1,20 @@
+# Total count of students enrolled in the course
+students_count = 1000
+
+# Course rating out of 5
+rating = 4.99
+
+# Flag to indicate if the course is published or not
+is_published = False
+
+# Course name that spans multiple lines
+course_name = """
+Multiple
+Lines
+"""
+
+# Assigning values to x and y variables
+x, y = 1, 2
+
+# Assigning the same value to both x and y
+x = y = 1
